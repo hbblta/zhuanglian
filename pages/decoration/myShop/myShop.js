7116d14ac82315d1fb53c5fd75cfba17959e2b71
hbblta <1758380410@qq.com>
2020-10-20 17:30:54 +0800

    hbb

--- pages/decoration/myShop/myShop.js
+++ pages/decoration/myShop/myShop.js
@@ -5,42 +5,68 @@ Page({
    * 页面的初始数据
    */
   data: {
-    shopTabList : [
-      {
-        title : '主页',
-        iconUrl : '../../../image/shopIcon/11.png',
-        changeIcon : '../../../image/shopIcon/1.png',
+    shopTabList: [{
+        title: '主页',
+        iconUrl: '../../../image/shopIcon/11.png',
+        changeIcon: '../../../image/shopIcon/1.png',
       },
       {
-        title : '大厅',
-        iconUrl : '../../../image/shopIcon/22.png',
-        changeIcon : '../../../image/shopIcon/2.png',
+        title: '大厅',
+        iconUrl: '../../../image/shopIcon/22.png',
+        changeIcon: '../../../image/shopIcon/2.png',
       },
       {
-        title : '消息',
-        iconUrl : '../../../image/shopIcon/33.png',
-        changeIcon : '../../../image/shopIcon/3.png',
+        title: '消息',
+        iconUrl: '../../../image/shopIcon/33.png',
+        changeIcon: '../../../image/shopIcon/3.png',
       },
       {
-        title : '推广',
-        iconUrl : '../../../image/shopIcon/44.png',
-        changeIcon : '../../../image/shopIcon/4.png',
+        title: '推广',
+        iconUrl: '../../../image/shopIcon/44.png',
+        changeIcon: '../../../image/shopIcon/4.png',
       },
       {
-        title : '会员',
-        iconUrl : '../../../image/shopIcon/55.png',
-        changeIcon : '../../../image/shopIcon/5.png',
+        title: '会员',
+        iconUrl: '../../../image/shopIcon/55.png',
+        changeIcon: '../../../image/shopIcon/5.png',
       },
-    ],
-    imgArr:[
-      {
-        imageUrl : 'https://dss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/logo_white-d0c9fe2af5.png'
+    ], //tabbar数组
+    imgArr: [{
+        imageUrl: 'https://dss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/logo_white-d0c9fe2af5.png'
       },
       {
-        imageUrl : 'https://dss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/logo_white-d0c9fe2af5.png'
+        imageUrl: 'https://dss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/logo_white-d0c9fe2af5.png'
       }
-  ],
-    tabIndex : 0
+    ],//轮播图数组
+    shopFeatures:[
+      {
+        title: '案例集经',
+        iconUrl: '../../../image/shopIcon/features1.png',
+        url:''
+      },
+      {
+        title: '团队介绍',
+        iconUrl: '../../../image/shopIcon/features2.png',
+        url:''
+      },
+      {
+        title: '免费设计',
+        iconUrl: '../../../image/shopIcon/features3.png',
+        url:''
+      },
+      {
+        title: '装修预算',
+        iconUrl: '../../../image/shopIcon/features4.png',
+        url:''
+      },
+      {
+        title: '材料商城',
+        iconUrl: '../../../image/shopIcon/features5.png',
+        url:''
+      },
+
+    ],
+    tabIndex: 0
   },
 
   /**
@@ -49,10 +75,13 @@ Page({
   onLoad: function (options) {
 
   },
-  changeIndex(e){
+  changeIndex(e) {
     this.setData({
-      tabIndex : e.currentTarget.dataset.index
+      tabIndex: e.currentTarget.dataset.index
     })
+  },
+  goUrl(e){
+    app.goUrl(e.currentTarget.dataset.url)
   },
   /**
    * 生命周期函数--监听页面初次渲染完成

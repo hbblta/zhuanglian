b3dae4554fef7d617bc02aa3c7fdbde826b563a8
1351985377@qq.com <1351985377@qq.com>
2020-10-20 17:28:01 +0800

    lwj

--- pages/decoration/personnelDetails/personnelDetails.js
+++ pages/decoration/personnelDetails/personnelDetails.js
@@ -6,7 +6,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    src:'',
+    name:'',
+    year:'',
+  },
+  getName(e){
+    this.setData({
+      name:e.detail.value
+    })
+  },
+  getYear(e){
+    this.setData({
+      year:e.detail.value
+    })
   },
 
   /**
@@ -18,6 +30,34 @@ Page({
   goUrl(e){
     app.goUrl(e.currentTarget.dataset.url)
   },
+
+  upload(){
+    var that = this
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        wx.uploadFile({
+          url: app.globalData.httpUrl + '/common/uploadimage',
+          filePath: res.tempFilePaths[0],
+          name: 'file',
+          header: {
+            "Authorization": app.globalData.Authorization
+          },
+          success: function (res) {
+            console.log(res)
+            if(res.statusCode == 200){
+              that.setData({
+                src:JSON.parse(res.data).data.FileUrl
+              })
+            }
+          }
+        })
+      }
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
--- pages/decoration/teamManagement/teamManagement.js
+++ pages/decoration/teamManagement/teamManagement.js
@@ -6,19 +6,39 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list : 10,
+    list : [],
     textList : [
       {
         name : '设计团队',
-        id : 0
+        id : 1
       },
       {
         name : '施工团队',
-        id : 1
+        id : 2
       },
     ],
+    //默认设计团队
+    type:1,
+    page:1,
+    total:0,
+    keyword:''
   },
-
+  getList(e){
+    var index = e.detail.index
+    this.setData({
+      type:this.data.textList[index].id,
+      page:1,
+      keyword:''
+    })
+    this.getUser()
+  },
+  inputvalue(e){
+    this.setData({
+      keyword:e.detail,
+      list:[],
+    })
+    this.getUser()
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -26,7 +46,36 @@ Page({
     wx.setNavigationBarTitle({
       title: '团队管理'
     })
+    this.getUser()
   },
+  //滚动条到底
+  scroll(e){
+    console.log(e)
+  },
+  //获取列表
+  getUser(){
+    var data={
+      type:this.data.type,
+      keyword:this.data.keyword,
+      page:this.data.page,
+      pagesize:10,
+    }
+    app.ajaxToken('/shop/getteamlist/'+app.globalData.userData.ShopID,data,'get').then(res=>{
+      console.log(res)
+      if(res.status == 0){
+        this.setData({
+          total:res.totalitem,
+          list:this.data.list.concat(res.data)
+        })
+      }else{
+        wx.showToast({
+          title:res.msg,
+          icon:'none'
+        })
+      }
+    })
+  },
+  
   goUrl(e){
     app.goUrl(e.currentTarget.dataset.url)
   },
